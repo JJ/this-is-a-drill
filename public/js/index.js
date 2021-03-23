e89492e87eeec2bc7ae27d314a2d796ab17b2fa6
JJ Merelo <jjmerelo@gmail.com>
2021-03-23 08:39:16 +0100

    Fix double quotes refs #2

--- public/js/index.js
+++ public/js/index.js
@@ -5,15 +5,15 @@ let stored = null;
 const digits = [...Array(10).keys()].map((key) => key.toString());
 
 const operations = {
-  "+": (first, second) => first + second,
-  "-": (first, second) => first - second,
-  "*": (first, second) => first * second,
-  "/": (first, second) => first / second
+  '+': (first, second) => first + second,
+  '-': (first, second) => first - second,
+  '*': (first, second) => first * second,
+  '/': (first, second) => first / second
 };
 
 const elements = {
   get display() {
-    return document.getElementById("display");
+    return document.getElementById('display');
   },
   digitButtons: (() => {
     const buttons = {};
@@ -25,10 +25,10 @@ const elements = {
     return buttons;
   })(),
   get separatorButton() {
-    return document.getElementById("btn-separator");
+    return document.getElementById('btn-separator');
   },
   get clearButton() {
-    return document.getElementById("btn-clear");
+    return document.getElementById('btn-clear');
   },
   operationButtons: (() => {
     const buttons = {};
@@ -36,33 +36,33 @@ const elements = {
       Object.defineProperty(buttons, opCode, {
         enumerable: true,
         get: () => document.getElementById({
-          "+": "btn-add",
-          "-": "btn-subtract",
-          "*": "btn-multiply",
-          "/": "btn-divide"
+          '+': 'btn-add',
+          '-': 'btn-subtract',
+          '*': 'btn-multiply',
+          '/': 'btn-divide'
         }[opCode])
       });
     return buttons;
   })(),
   get calculateButton() {
-    return document.getElementById("btn-calculate");
+    return document.getElementById('btn-calculate');
   }
 }
 
 function setUpEntryButtons() {
   for (let [digit, button] of Object.entries(elements.digitButtons))
-    button.addEventListener("click", function() {
+    button.addEventListener('click', function() {
       elements.display.textContent += digit;
     });
 
-  elements.separatorButton.addEventListener("click", function() {
+  elements.separatorButton.addEventListener('click', function() {
     const text = elements.display.textContent;
-    if (text.length && text.indexOf(".") === -1)
-      elements.display.textContent += ".";
+    if (text.length && text.indexOf('.') === -1)
+      elements.display.textContent += '.';
   });
 
-  elements.clearButton.addEventListener("click", function() {
-    elements.display.textContent = "";
+  elements.clearButton.addEventListener('click', function() {
+    elements.display.textContent = '';
     stored = null;
   });
 }
@@ -75,17 +75,17 @@ function calculate() {
 
 function setUpOperationButtons() {
   for (let [opCode, button] of Object.entries(elements.operationButtons))
-    button.addEventListener("click", function() {
+    button.addEventListener('click', function() {
       stored = {
         text: stored ? calculate() : elements.display.textContent,
         opCode
       };
-      elements.display.textContent = "";
+      elements.display.textContent = '';
     });
 }
 
 function setUpCalculateButton() {
-  elements.calculateButton.addEventListener("click", function() {
+  elements.calculateButton.addEventListener('click', function() {
     if (!stored)
       return;
     elements.display.textContent = calculate();
