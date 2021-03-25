020a95b697b0f1fbf085eb58d0057e0ff5232312
JJ Merelo <jjmerelo@gmail.com>
2021-03-25 08:43:37 +0100

    Checks interaction

--- cypress/integration/ui_spec.js
+++ cypress/integration/ui_spec.js
@@ -18,16 +18,37 @@ describe('The page exists and buttons can be clicked', () => {
         cy.get(`#${BUTTON_PREFIX}-${digit}`).click()
     }
     cy.get("#display").contains("0123456789")
-    cy.get("#btn-clear").click()
+    cy.get(btnName('clear')).click()
     cy.get("#display").contains(/^$/)
   })
 
   it('Avoids double separators', () => {
-    cy.get("#btn-1").click()
-    cy.get("#btn-separator").click()
-    cy.get("#btn-1").click()
-    cy.get("#btn-separator").click()
+    cy.get(btnName(1)).click()
+    cy.get(btnName('separator')).click()
+    cy.get(btnName(1)).click()
+    cy.get(btnName('separator')).click()
     cy.get("#display").contains(/^1\.1$/)
+  })
+
+  it('Performs operations correctly', () => {
+    const NUMBER = 42
+    for (const opCode of Object.keys(operations)) {
+      clickStream(NUMBER)
+      cy.get(btnName(operations[opCode][0])).click()
+      clickStream(NUMBER)
+      cy.get(btnName('calculate')).click()
+      cy.get("#display").contains( eval ( NUMBER.toString() + opCode + NUMBER.toString() ))
+    }
   })
 })
 
+function btnName( arg ) {
+    return `#${BUTTON_PREFIX}-${arg}`
+}
+
+function clickStream( arg ) {
+  let str = typeof arg == 'string' ? arg : arg.toString()
+  for ( let c of str[Symbol.iterator]() ) {
+    cy.get( btnName( c ) ).click()
+  }
+}
