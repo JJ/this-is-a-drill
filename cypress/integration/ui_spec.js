7f1f5baa9feea1ebb5e01a5a2d7f28ac43c183e2
JJ Merelo <jjmerelo@gmail.com>
2021-03-25 07:45:13 +0100

    Adds deletion before each test #4

--- cypress/integration/ui_spec.js
+++ cypress/integration/ui_spec.js
@@ -4,6 +4,7 @@ describe('The page exists and buttons can be clicked', () => {
 
   beforeEach(() => {
      cy.visit('public/index.html')
+     cy.get("#btn-clear").click
   })
 
   it('Check out the calculator', () => {
@@ -11,9 +12,7 @@ describe('The page exists and buttons can be clicked', () => {
         cy.get(`#${BUTTON_PREFIX}-${digit}`).contains(digit)
     }
   })
-})
 
-describe('Calculator works', () => {
   it('Can click on numbers and they show on screen', () => {
     for (const digit of digits) {
         cy.get(`#${BUTTON_PREFIX}-${digit}`).click()
