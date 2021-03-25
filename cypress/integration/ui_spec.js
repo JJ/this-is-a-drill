d22b1352bf15dc49976202616ceff996c7039ee7
JJ Merelo <jjmerelo@gmail.com>
2021-03-25 07:37:27 +0100

    Refactoring tests to take make them follow best practices
    Like this one
    https://docs.cypress.io/guides/references/best-practices#Having-tests-rely-on-the-state-of-previous-tests
    
    Refs #4

--- cypress/integration/ui_spec.js
+++ cypress/integration/ui_spec.js
@@ -1,12 +1,24 @@
 import { BUTTON_PREFIX, digits, elements, operations } from '../../public/js/constants.js'
 
-describe('The page exists', () => {
+describe('The page exists and buttons can be clicked', () => {
+
+  beforeEach(() => {
+     cy.visit('public/index.html')
+  })
+
   it('Check out the calculator', () => {
-    cy.visit('public/index.html')
     for (const digit of digits) {
         cy.get(`#${BUTTON_PREFIX}-${digit}`).contains(digit)
     }
   })
 })
 
+describe('Calculator works', () => {
+  it('Can click on numbers and they show on screen', () => {
+    for (const digit of digits) {
+        cy.get(`#${BUTTON_PREFIX}-${digit}`).click()
+    }
+    cy.get("#display").contains("0123456789")
+  })
+})
 
